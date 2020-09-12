df25a5ea42d2387afe80251cb7e1240d64eae75d
Liam Hanafee-Areces <liamhanafee@gmail.com>
2020-09-12 21:00:33 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -34,18 +34,7 @@ def initial_round
 end
 
 
-#def hit?(card_total)
- # prompt_user
-  #user_input = get_user_input
-#  if user_input == "h"
- #     card_total += deal_card
-  #  elsif user_input == "s"
-  #    card_total
-  #  else
-  #    invalid_command
-  #    get_user_input
-#  end  
-#end
+
 
 def hit?(card_total)
   prompt_user
@@ -70,6 +59,12 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(card_total)
 end
     
